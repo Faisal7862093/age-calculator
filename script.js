d48fae8b30841e973b7eef24fd279067c279de0e
Faisal7862093 <muhammadfaisal7862093@gmail.com>
2024-09-29 10:45:27 +0500

    years, months and days are get using get function

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 function calculation() {
     var setdate = new Date(document.getElementById("setdate").value);
-    var today = new Date();
+    var todaydate = new Date();
 
+    var years = todaydate.getFullYear - setdate.getFullYear;
+    var months = todaydate.getMonth - setdate.getMonth;
+    var days = todaydate.getDay - setdate.getDay;
     
 }
